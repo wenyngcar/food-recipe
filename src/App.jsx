735017774b488ed62a4a7c6b95f2f4362caafb4b
wenyngcar <owenyoungcarino@gmail.com>
2025-05-06 23:29:00 +0800

    feat: add custom to fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useFetchMeal } from "./api/mealDB"
+
 function App() {
+
+  // Call custom hook to fetch all meal.
+  useFetchMeal()
 
   return (
     <>
       <div className='bg-blue-300'>test</div>
+      <div>
+
+      </div>
     </>
   )
 }

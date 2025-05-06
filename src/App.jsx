d082cc9c1aadbd49e674a49b370df14d95c862b7
wenyngcar <owenyoungcarino@gmail.com>
2025-05-06 23:46:31 +0800

    feat: add shadcn card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useFetchMeal } from "./api/mealDB"
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 function App() {
 
   // Call custom hook to fetch all meal.
@@ -9,7 +16,16 @@ function App() {
     <>
       <div className='bg-blue-300'>test</div>
       <div>
-
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>Deploy your new project in one-click.</CardDescription>
+          </CardHeader>
+          <CardContent>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+          </CardFooter>
+        </Card>
       </div>
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'

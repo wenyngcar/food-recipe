548078e7f4b259b58d97f44869b439703730b7a3
Owen Cariño <owenyoungcarino@gmail.com>
2025-05-08 11:42:04 +0800

    feat: Filter by category(#6)
    * fix: conflict with header
    
    * feat: display meal by category
    
    * style: same style with home page
    
    * fix: add key for each category

--- src/api/filterByCategory.ts
+++ src/api/filterByCategory.ts
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react"
 
-export default function mealDetails(id: string) {
-  const apiUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i="
+export function FilterByCategory(category: string) {
+  const apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
+
   const [isLoading, setIsLoading] = useState(false)
   const [meal, setMeal] = useState([])
 
   const searchRecipes = async () => {
     setIsLoading(true)
-    const res = await fetch(apiUrl + id)
+    const res = await fetch(apiUrl + category)
     const data = await res.json()
-    setMeal(data.meals[0])
+    setMeal(data.meals)
     setIsLoading(false)
-    // console.log(data.meals[0])
+    // console.log(data.meals)
   }
 
   // Call this first when FetchMeal is called. 
   useEffect(() => {
     searchRecipes()
-  }, [])
+  }, [category])
 
   return { meal, isLoading }
-
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
-import { Link, Outlet } from "react-router-dom";
-
+import { ListOfCategory } from "@/api/listOfCategory"
+import { Link, Outlet } from "react-router-dom"
 export default function Header() {
+  const { categories } = ListOfCategory()
   return (
     <>
-      <header className="bg-gradient-to-r from-[#00b33c] via-[#ccffcc] to-[#ffff99] p-6 flex items-center justify-between shadow-xl">
-      
+      <header className="bg-gradient-to-r from-[#00b33c] via-[#ccffcc] to-[#ffff99] p-6 px-[10%] flex items-center justify-between shadow-xl">
         <div className="flex items-center space-x-6">
           <div className="w-32 h-32 rounded-full flex items-center justify-center transition duration-300 hover:scale-105 hover:shadow-[0_0_30px_15px_rgba(255,255,255,0.8)]">
             <img
@@ -13,8 +13,6 @@ export default function Header() {
               className="w-40 h-40 object-contain"
             />
           </div>
-
-  
           <Link
             to="/home"
             className="text-white text-5xl font-semibold font-serif tracking-wider hover:text-white hover:shadow-white transition"
@@ -22,9 +20,24 @@ export default function Header() {
             <h4>Undecided</h4>
           </Link>
         </div>
-      </header>
 
-      <main className="px-[15%]">
+        <div className="flex space-x-10 text-lg text-gray-600 font-semibold">
+          <Link to={"/home"}>
+            Home
+          </Link>
+          <div className="relative group/category">
+            <p>Category</p>
+            <div className="absolute bg-slate-100 rounded group-hover/category:opacity-100 opacity-0 transition ease-in-out duration-300">
+              {categories.map((meal, key) => (
+                <Link to={`/home/${meal["strCategory"]}`} key={key}>
+                  <p className="cursor-pointer hover:bg-gray-300 p-2 transition ease-in-out duration-300" key={key}>{meal["strCategory"]}</p>
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="px-[15%]" >
         <Outlet />
       </main>
     </>

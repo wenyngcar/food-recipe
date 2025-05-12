5f61871936c220bcd17fdb199cd867bb59bb3a7a
wenyngcar <owenyoungcarino@gmail.com>
2025-05-12 10:53:37 +0800

    refactor: add home component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route, Navigate, data } from "react-router-dom"
+import Header from './components/Header.tsx'
+import Home from './routes/Home.tsx'
+import FoodDetails from './routes/FoodDetails.tsx'
+import FilteredPage from './routes/FilteredPage.tsx'
+
+function App() {
+
+
+  return (
+    <Routes>
+      <Route element={<Header />}>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route index path='/home' element={<Home />} />
+        <Route path='/food-details/:mealId' element={<FoodDetails />} />
+        <Route path='/home/:category' element={<FilteredPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/routes/Home.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "../components/LandingPage"
+import SearchBar from "../components/SearchBar"
+
+export default function Home() {
+
+  return (
+    <div className="-mt-24">
+      <LandingPage />
+      <SearchBar />
+    </div>
+  )
+}
